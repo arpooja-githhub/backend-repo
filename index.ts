5e1c73541776a73fd7350831e2fa2d4dbd096192
Pooja A R <pooja.a.r.2005@gmail.com>
2026-04-30 18:58:59 +0530

    working backend ready for render deployments

--- index.ts
+++ index.ts
@@ -268,5 +268,5 @@ app.use((req, res) => {
   res.status(404).json({ error: `Route ${req.method} ${req.path} not found` });
 });
 
-const PORT = parseInt(process.env.PORT ?? "5000");
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));

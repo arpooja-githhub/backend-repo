05281ae0b722d18cfeea10259f1fb2b5121438dd
Pooja A R <pooja.a.r.2005@gmail.com>
2026-04-30 20:48:49 +0530

    fix typescript build errors for renders

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 
 const { Pool } = pkg;
 dotenv.config();
-console.log("DB:", process.env.DATABASE_URL); 
+
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
